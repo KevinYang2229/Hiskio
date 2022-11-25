c0d5f2dd878970c4910fe31e74a20fbb31a7a68d
Kevin <kevin_yang@systemweb.com.tw>
2022-11-26 00:40:53 +0800

    ✨feature: init

---
+++ layouts/StandardLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <BaseHeader />
+    <div class="flex justify-center w-full bg-neutral-100 min-h-screen sm:pt-20">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'SimpleLayout',
+  scrollToTop: true,
+  computed: {
+    ...mapState({
+      loading: state => state.loading,
+    }),
+  },
+}
+</script>
