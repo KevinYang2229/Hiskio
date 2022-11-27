34091bc93b50dea6ce1f9637644b744cb35f25c2
Kevin <kevin_yang@systemweb.com.tw>
2022-11-27 19:05:07 +0800

    🆙update: 新增登入後狀態

--- store/index.js
+++ store/index.js
@@ -1,8 +1,12 @@
 export const state = () => ({
+  isOpenLogin: false,
   isLogin: false,
 })
 
 export const mutations = {
+  setIsOpenLogin(state, stateValue) {
+    state.isOpenLogin = stateValue
+  },
   setIsLogin(state, stateValue) {
     state.isLogin = stateValue
   },
