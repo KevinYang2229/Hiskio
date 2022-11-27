336a938df8f1e8f2281cb0f2987b437064ff92dd
Kevin <kevin_yang@systemweb.com.tw>
2022-11-27 19:07:24 +0800

    🆙update: 首頁新增修改item功能開發

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,8 @@
       <div class="mb-8 sm:mr-4 md:flex-1">
         <h2 class="font-bold text-xl mb-5">購物車</h2>
         <div class=" bg-white shadow-borderBottom">
-          <CardList class="sm:hidden block"></CardList>
-          <TableList class="hidden sm:block"></TableList>
+          <CardList class="sm:hidden block" :carts="cartList" @delete="deleteRec"></CardList>
+          <TableList class="hidden sm:block" :carts="cartList" @delete="deleteRec"></TableList>
         </div>
       </div>
 
@@ -14,7 +14,7 @@
         <div class="bg-white p-3 rounded-lg shadow-borderBottom">
           <div class="border-b mb-4">
             <div class="text-gray-500">輸入折扣代碼</div>
-            <input class="border rounded w-full h-10" type="text">
+            <input class="border rounded w-full h-10 px-2" type="text">
             <div class="flex items-center mt-5 mb-20 sm:mb-3">
               <a class="mr-[2px] text-gray-600 underline underline-offset-2" href="#">選擇抵用卷</a>
               <svg width="15" height="12" viewBox="0 0 15 12" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -26,9 +26,9 @@
           <div>
             <div class="flex justify-between mb-12 sm:mb-3">
               <span class="text-gray-500">金額</span>
-              <span class="text-gray-600">$7,960</span>
+              <span class="text-gray-600">${{ totalAmt.price }}</span>
             </div>
-            <div class="text-3xl text-right mb-4">$7,060</div>
+            <div class="text-3xl text-right mb-4">${{ totalAmt.price }}</div>
             <button class="bg-secondary-default text-white w-full p-3 rounded mb-3">前往結帳</button>
             <small class="text-gray-500 text-xs">點擊上方按鈕即表示您已閱讀並同意「 HiSKIO購買與退費政策」</small>
           </div>
@@ -40,13 +40,13 @@
       <h2 class="font-bold text-xl mb-3">募資課程</h2>
       <div class="block sm:flex sm:gap-3">
         <div v-for="(item, i) in data" :key="item.id" >
-          <MobileCard class="sm:hidden block" :idx="i" :product="item"></MobileCard>
-          <TableCard class="hidden sm:block" :idx="i" :product="item"></TableCard>
+          <MobileCard class="sm:hidden block cursor-pointer" :idx="i" :product="item" @click.native="add(item)"></MobileCard>
+          <TableCard class="hidden sm:block cursor-pointer" :idx="i" :product="item" @click.native="add(item)"></TableCard>
         </div>
       </div>
     </div>
 
-    <Login v-show="isLogin"></Login>
+    <Login v-show="isOpenLogin"></Login>
   </div>
 
 </template>
@@ -69,15 +69,28 @@ export default {
   },
   data() {
     return {
+      cartList: []
     }
   },
   computed: {
     ...mapState({
-      isLogin: state => state.isLogin,
+      isOpenLogin: state => state.isOpenLogin,
     }),
+    totalAmt() {
+      return this.cartList.reduce((accumulator, currentValue) => {
+        return {price: Number(accumulator.price) + Number(currentValue.price)}
+      }, {price: 0})
+    }
   },
-  created() {
-    console.log(this.data)
-  }
+  methods: {
+    add(item) {
+      const matchItem = this.cartList.find(cart => cart.id === item.id)
+      matchItem ? this.deleteRec(item.id) : this.cartList.push(item)
+    },
+    deleteRec(e) {
+      const currIndex = this.cartList.findIndex(cart => cart.id === e)
+      this.cartList.splice(currIndex, 1)
+    }
+  },
 }
 </script>
